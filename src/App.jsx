21acc71a80694b880ab110723a6e19f669d624e6
Yuri <yuri.kamau@student.moringaschool.com>
2024-05-30 10:17:07 +0300

    stylings

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import RatingsList from './assets/components/RatingsList';
+import './index.css'
 
 const App = () => {
   return (
--- src/assets/components/RatingsList.jsx
+++ src/assets/components/RatingsList.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import Rating from './Rating.jsx';
- import AverageRating from './AverageRating.jsx';
+import Rating from './Rating';
+import AverageRating from './AverageRating';
 
 function RatingsList() {
   const [name, setName] = useState('');
@@ -9,32 +9,37 @@ function RatingsList() {
   const [ratings, setRatings] = useState([]);
 
   function handleRate() {
-    setRatings([...ratings, { name, content, rate }]);
-    setName('');
-    setContent('');
-    setRate(0);
+    if (name && content && rate) {
+      setRatings([...ratings, { name, content, rate }]);
+      setName('');
+      setContent('');
+      setRate(0);
+    } else {
+      alert('Please fill in all fields.');
+    }
   }
 
   return (
     <div className="ratings">
-      <h1>Rating List</h1>
-      <div>
-        <label>Name:</label>
-        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-      </div>
-      <div>
-        <label>Content:</label>
-        <input type="text" value={content} onChange={(e) => setContent(e.target.value)} />
-      </div>
-      <div>
-        <label>Rating:</label>
-        <input
-          type="number"
-          value={rate}
-          min={1}
-          max={5}
-          onChange={(e) => setRate(parseInt(e.target.value))}
-        />
+      <div className="form-row">
+        <div className="form-group">
+          <label>Name:</label>
+          <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        </div>
+        <div className="form-group">
+          <label>Content:</label>
+          <input type="text" value={content} onChange={(e) => setContent(e.target.value)} />
+        </div>
+        <div className="form-group">
+          <label>Rating:</label>
+          <input
+            type="number"
+            value={rate}
+            min={1}
+            max={5}
+            onChange={(e) => setRate(parseInt(e.target.value))}
+          />
+        </div>
       </div>
       <button onClick={handleRate}>Submit Rating</button>
       <AverageRating ratings={ratings} />
